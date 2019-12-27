56f3d0bb216587886a739735996b2e4d8ffd16dd
x232209 <gilson.bezerra@prservicos.com.br>
2019-12-26 22:34:03 -0300

    Apagando a pagina List

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+// Imports core Ionic
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePage } from './home/home.page';
+
+// Imports páginas
+
+
+
+@NgModule({
+  imports: [
+    // Módulos do Ionic
+    CommonModule
+
+    // Ferramentas Utilizadas
+
+
+  ],
+  declarations: [
+    HomePage
+  ]
+})
+export class PagesModule { }
